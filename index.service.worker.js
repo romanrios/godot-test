d30a8858dfdc3fabfd4be91f0b18dfdf4a3a7543
Román Ríos <romanrios@live.com>
2025-01-30 08:26:54 -0300

    wip

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1738235179|99477627';
+const CACHE_VERSION = '1738236387|417520255';
 /** @type {string} */
 const CACHE_PREFIX = 'Starter Kit 3D P-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
